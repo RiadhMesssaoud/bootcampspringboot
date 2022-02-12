8bff72ca222ab77e8cde8806d62072d5efb889cf
Riadh Messaoud <messaoudriadh001@gmail.com>
2022-02-12 13:56:17 +0100

    premier projet MVC

---
+++ src/main/java/com/SIP/ams/controllers/HomeControllers.java
@@ -0,0 +1,27 @@
+package com.SIP.ams.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeControllers {
+@RequestMapping("/info")
+   public String info()
+   {
+	System.out.println("methode info");
+	   return "home/info" ;
+   }
+@RequestMapping("/affiche")
+   public String affichage()
+	   {
+		return "home/affiche" ;
+	   }
+
+
+@RequestMapping("/information")
+public String information()
+	   {
+		return "home/information" ;
+	   }
+
+}
